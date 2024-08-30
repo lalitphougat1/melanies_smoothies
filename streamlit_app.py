b3cfcbc88e3fd9463bec066191a4442542d3624c
lalitphougat1 <lalit_phougat@yahoo.co.in>
2024-08-30 15:57:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ if ingredients_list:
 
 	for fruit_chosen in ingredients_list:
         	ingredients_string += fruit_chosen + ' '
-   	my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order) 
+   	my_insert_stmt = """insert into smoothies.public.orders(ingredients, name_on_order) 
     	values ('""" + ingredients_string + """','""" + name_on_order + """' )"""
 
     	time_to_insert = st.button('Submit Order')
