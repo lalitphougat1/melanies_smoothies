85b005f69cd1eacf65a7205a4655a9e02bcc2c02
lalitphougat1 <lalit_phougat@yahoo.co.in>
2024-08-30 11:09:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 #import get_active_session
+from snowflake.snowpark import session
 # from snowflake.snowpark.functions import col
 
 # Write directly to the app
