c206258426db0d3100d9133276384cc613a14222
lalitphougat1 <lalit_phougat@yahoo.co.in>
2024-08-30 16:26:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,11 @@ if ingredients_list:
 
 	for fruit_chosen in ingredients_list:
         	ingredients_string += fruit_chosen + ' '
+		
+		st.subheader(fruit_chosen + 'Nutrition Information')
+		fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
+		fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+		
 	my_insert_stmt = """insert into smoothies.public.orders(ingredients, name_on_order) 
     	values ('""" + ingredients_string + """','""" + name_on_order + """' )"""
 	time_to_insert = st.button('Submit Order')
@@ -31,7 +36,7 @@ if ingredients_list:
             st.success('Your Smoothie is ordered!', icon="✅")
 	st.write(my_insert_stmt)
 
-	st.subheader(fruit_chosen + 'Nutrition Information')
-	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
-	fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+	#st.subheader(fruit_chosen + 'Nutrition Information')
+	#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
+	#fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 st.stop()
