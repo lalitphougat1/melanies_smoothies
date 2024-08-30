622eb077239e86d7e9eee6f6d0c6a973a1f8a494
lalitphougat1 <lalit_phougat@yahoo.co.in>
2024-08-30 16:22:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,6 @@ if ingredients_list:
 	st.write(my_insert_stmt)
 
 	st.subheader(fruit_chosen + 'Nutrition Information')
-		fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
+	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
 		fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 st.stop()
